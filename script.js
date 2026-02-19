a8d79732436bdc96c1910b1ea827a5d97f83ba38
Cassio-Penna <cassiodelimarp@gmail.com>
2026-02-18 21:30:46 -0300

    Update Instagram link in irParaInstagram function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function irParaInstagram() {
   window.open(
-    "https://www.instagram.com/110.gemar.marica?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==",
+    "https://www.instagram.com/110.gemar.marica?igsh=MTBjc2plbjZnbndsYg== ",
     "_blank"
   );
 }
+
